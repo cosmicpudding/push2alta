99a2c03eb3b4fc691165b308dc719cbcd3e668f8
Vanessa Moss <rhobslein63@gmail.com>
2018-07-11 11:49:48 +0200

    Debug

--- generate_alta.py
+++ generate_alta.py
@@ -34,7 +34,7 @@ for ds in datasets:
 		# Assume it is not in ALTA or will be dealt with at a later stage...
 		os.system('mkdir ../transfer/WSRTA%s' % tid)
 		os.chdir('../transfer/WSRTA%s' % tid)
-		os.system("../prepare_transfer_alta.sh -s /data/apertif/%s -f '.*%s.*' -i 'icat,res1' -1" % (ds,tid))
+		os.system("../../prepare_transfer_alta.sh -s /data/apertif/%s -f '.*%s.*' -i 'icat,res1' -1" % (ds,tid))
 
 		sys.exit()
 	
