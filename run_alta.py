723ffbf33531fd7b84f5773e86325930c57cb897
Vanessa Moss <rhobslein63@gmail.com>
2018-07-11 11:58:05 +0200

    Add a script to run the generated files

---
+++ run_alta.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python2.7
+
+# APERTIF HAPPILI > ALTA SCRIPT RUNNER (run_alta.py)
+# V.A. Moss 11/07/2018 (vmoss.astro@gmail.com)
+__author__ = "V.A. Moss"
+__date__ = "$11-jul-2018 12:00:00$"
+__version__ = "1.0"
+
+import os
+import sys
+
+path = '../transfer/'
+
+# Get list of possible datasets
+datasets = []
+cmd = os.popen('ls %s/*' % path)
+for x in cmd:
+	folder = x.strip()
+	datasets.append(folder)
+
+print datasets
+
+sys.exit()
